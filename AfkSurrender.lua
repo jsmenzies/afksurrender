b37b5aa06515802dc470d09e6a0e6f93d007d90a
James Menzies <james@menzies.io>
2025-12-31 22:51:27 +0000

    updated

--- AfkSurrender.lua
+++ AfkSurrender.lua
@@ -1,13 +1,10 @@
 local function OnSlashCommand(msg)
-    -- Check if we're in an arena
     local instanceType = select(2, IsInInstance())
     
     if instanceType == "arena" then
-        -- We're in arena, surrender instead
         C_PvP.SurrenderArena()
         print("Surrendering arena...")
     else
-        -- Not in arena, do normal AFK
         SendChatMessage("", "AFK")
         print("Normal AFK (not in arena)")
     end
