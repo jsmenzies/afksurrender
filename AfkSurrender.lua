cdd1f941050571e97ae1e613ba7fd1fd9c22e9f9
James Menzies <james@menzies.io>
2026-01-01 03:52:09 +0000

    Fix /afk command interception

--- AfkSurrender.lua
+++ AfkSurrender.lua
@@ -1,18 +1,19 @@
-local function OnSlashCommand(msg)
-    local instanceType = select(2, IsInInstance())
-    
-    if instanceType == "arena" then
-        C_PvP.SurrenderArena()
-        print("Surrendering arena...")
-    else
-        SendChatMessage("", "AFK")
-        print("Normal AFK (not in arena)")
+local OriginalSendChatMessage = SendChatMessage
+
+function SendChatMessage(msg, chatType, language, target)
+    if chatType == "AFK" then
+        local instanceType = select(2, IsInInstance())
+
+        if instanceType == "arena" then
+            C_PvP.SurrenderArena()
+            print("AfkSurrender: Surrendering arena!")
+            return
+        else
+            print("AfkSurrender: Going AFK")
+        end
     end
+
+    return OriginalSendChatMessage(msg, chatType, language, target)
 end
 
--- Register the slash command
-SLASH_AFK1 = "/afk"
-SlashCmdList["AFK"] = OnSlashCommand
-
--- Simple direct print
-DEFAULT_CHAT_FRAME:AddMessage("AFK Surrender addon loaded!", 1, 1, 0)
+DEFAULT_CHAT_FRAME:AddMessage("AfkSurrender loaded!", 1, 1, 0)
